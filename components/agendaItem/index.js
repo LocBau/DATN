36da90762192638ac1b4aa2bc5f49754753dca50
Bauauto <bauauto@gmail.com>
2023-11-09 16:20:29 +0700

    Finish Calendar

--- components/agendaItem/index.js
+++ components/agendaItem/index.js
@@ -1,46 +1,55 @@
-import React, {useCallback} from 'react';
-import {StyleSheet, Alert, View, Text, TouchableOpacity, Button} from 'react-native';
-
-
-
-
+import React, { useCallback } from "react";
+import {
+  StyleSheet,
+  Alert,
+  View,
+  Text,
+  TouchableOpacity,
+  Button,
+  Tooltip,
+} from "react-native";
+import {
+  MaterialCommunityIcons,
+  FontAwesome,
+  Ionicons,
+} from "@expo/vector-icons";
 const AgendaItem = (props) => {
-  const {item, info} = props;
+  const { item, info } = props;
   // console.log(item);
 
   const testIDs = {
     menu: {
-      CONTAINER: 'menu',
-      CALENDARS: 'calendars_btn',
-      CALENDAR_LIST: 'calendar_list_btn',
-      HORIZONTAL_LIST: 'horizontal_list_btn',
-      AGENDA: 'agenda_btn',
-      EXPANDABLE_CALENDAR: 'expandable_calendar_btn',
-      WEEK_CALENDAR: 'week_calendar_btn',
-      TIMELINE_CALENDAR: 'timeline_calendar_btn',
-      PLAYGROUND: 'playground_btn'
+      CONTAINER: "menu",
+      CALENDARS: "calendars_btn",
+      CALENDAR_LIST: "calendar_list_btn",
+      HORIZONTAL_LIST: "horizontal_list_btn",
+      AGENDA: "agenda_btn",
+      EXPANDABLE_CALENDAR: "expandable_calendar_btn",
+      WEEK_CALENDAR: "week_calendar_btn",
+      TIMELINE_CALENDAR: "timeline_calendar_btn",
+      PLAYGROUND: "playground_btn",
     },
     calendars: {
-      CONTAINER: 'calendars',
-      FIRST: 'first_calendar',
-      LAST: 'last_calendar'
+      CONTAINER: "calendars",
+      FIRST: "first_calendar",
+      LAST: "last_calendar",
     },
-    calendarList: {CONTAINER: 'calendarList'},
-    horizontalList: {CONTAINER: 'horizontalList'},
+    calendarList: { CONTAINER: "calendarList" },
+    horizontalList: { CONTAINER: "horizontalList" },
     agenda: {
-      CONTAINER: 'agenda',
-      ITEM: 'item'
+      CONTAINER: "agenda",
+      ITEM: "item",
     },
-    expandableCalendar: {CONTAINER: 'expandableCalendar'},
-    weekCalendar: {CONTAINER: 'weekCalendar'}
+    expandableCalendar: { CONTAINER: "expandableCalendar" },
+    weekCalendar: { CONTAINER: "weekCalendar" },
   };
   const buttonPressed = useCallback(() => {
     info(item);
   }, []);
 
-  const itemPressed = useCallback(() => {
-    info(item);
-  }, []);
+  // const itemPressed = useCallback(() => {
+  //   info(item);
+  // }, []);
 
   if (!item) {
     return (
@@ -50,59 +59,103 @@ const AgendaItem = (props) => {
     );
   }
 
+  const viewSource = () => {
+    // console.log(props.item.title);
+    // cho nay em tạo them 1 props 'item.source' nào để biết từ GGCalendar bỏ dzo đây nhé!
+    // code nay a để ví dụ cho item.title thôi..!!
+    if (props.item.title == "GGcalendar") {
+      return (
+        <View style={styles.viewGcalendar}>
+          <Ionicons
+            name="logo-google"
+            size={20}
+            color="green"
+            onPress={buttonPressed}
+          />
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.viewInfo}>
+          <FontAwesome
+            name="info-circle"
+            size={20}
+            color="purple"
+            onPress={buttonPressed}
+          />
+        </View>
+      );
+    }
+  };
+
   return (
-    <TouchableOpacity onPress={itemPressed} style={styles.item} testID={testIDs.agenda.ITEM}>
-      <View>
+    <View style={styles.container}>
+      {/* <TouchableOpacity onPress={itemPressed} testID={testIDs.agenda.ITEM}> */}
+      <View style={styles.viewTime}>
         <Text style={styles.itemHourText}>{item.hour}</Text>
-        {/* <Text style={styles.itemDurationText}>{item.duration}</Text> */}
       </View>
-      <Text style={styles.itemTitleText}>{item.title}</Text>
-      <View style={styles.itemButtonContainer}>
-        <Button color={'grey'} title={'Info'} onPress={buttonPressed}/>
+      <View style={styles.viewTitle}>
+        <Text style={styles.itemTitleText}>{item.title}</Text>
       </View>
-    </TouchableOpacity>
+
+      <View style={styles.viewSource}>{viewSource()}</View>
+      {/* </TouchableOpacity> */}
+    </View>
   );
 };
 
 export default AgendaItem;
 
-
 const styles = StyleSheet.create({
-  item: {
-    padding: 20,
-    backgroundColor: 'white',
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: "white",
     borderBottomWidth: 1,
-    borderBottomColor: 'lightgrey',
-    flexDirection: 'row'
+    borderBottomColor: "lightgrey",
+    flexDirection: "row",
+  },
+  viewTime: {
+    flex: 1,
   },
   itemHourText: {
-    color: 'black'
+    color: "black",
+  },
+  viewTitle: {
+    flex: 6,
   },
   itemDurationText: {
-    color: 'grey',
+    color: "grey",
     fontSize: 12,
     marginTop: 4,
-    marginLeft: 4
+    marginLeft: 4,
   },
   itemTitleText: {
-    color: 'black',
-    marginLeft: 16,
-    fontWeight: 'bold',
-    fontSize: 16
+    color: "black",
+    marginLeft: 10,
+    fontWeight: "bold",
+    fontSize: 14,
   },
-  itemButtonContainer: {
+  viewSource: {
     flex: 1,
-    alignItems: 'flex-end'
+    alignItems: "center",
+  },
+  viewGcalendar: {
+    flex: 1,
+  },
+  viewInfo: {
+    flex: 1,
+    alignItems: "center",
   },
   emptyItem: {
     paddingLeft: 20,
     height: 52,
-    justifyContent: 'center',
+    justifyContent: "center",
     borderBottomWidth: 1,
-    borderBottomColor: 'lightgrey'
+    borderBottomColor: "lightgrey",
   },
   emptyItemText: {
-    color: 'lightgrey',
-    fontSize: 14
-  }
+    color: "lightgrey",
+    fontSize: 14,
+  },
 });
--- screens/Calendar/index.js
+++ screens/Calendar/index.js
@@ -1,237 +1,266 @@
-import React, {useRef, useCallback, useState, useEffect} from 'react';
-import {StyleSheet, View,Text} from 'react-native';
-import {ExpandableCalendar, AgendaList, CalendarProvider, WeekCalendar} from 'react-native-calendars';
-import { Button, Card } from 'react-native-paper';
-import AgendaItem from '../../components/agendaItem/index'
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useRef, useCallback, useState, useEffect } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import {
+  ExpandableCalendar,
+  AgendaList,
+  CalendarProvider,
+  WeekCalendar,
+} from "react-native-calendars";
+import { Button, Card } from "react-native-paper";
+import AgendaItem from "../../components/agendaItem/index";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { MaterialCommunityIcons, FontAwesome } from "@expo/vector-icons";
 
+const Calendar = ({ navigation, route }) => {
+  const testIDs = {
+    menu: {
+      CONTAINER: "menu",
+      CALENDARS: "calendars_btn",
+      CALENDAR_LIST: "calendar_list_btn",
+      HORIZONTAL_LIST: "horizontal_list_btn",
+      AGENDA: "agenda_btn",
+      EXPANDABLE_CALENDAR: "expandable_calendar_btn",
+      WEEK_CALENDAR: "week_calendar_btn",
+      TIMELINE_CALENDAR: "timeline_calendar_btn",
+      PLAYGROUND: "playground_btn",
+    },
+    calendars: {
+      CONTAINER: "calendars",
+      FIRST: "first_calendar",
+      LAST: "last_calendar",
+    },
+    calendarList: { CONTAINER: "calendarList" },
+    horizontalList: { CONTAINER: "horizontalList" },
+    agenda: {
+      CONTAINER: "agenda",
+      ITEM: "item",
+    },
+    expandableCalendar: { CONTAINER: "expandableCalendar" },
+    weekCalendar: { CONTAINER: "weekCalendar" },
+  };
 
+  const theme = {
+    // arrows
+    arrowColor: "black",
+    arrowStyle: { padding: 0 },
+    // knob
+    expandableKnobColor: "#00AAAF",
+    // month
+    monthTextColor: "black",
+    textMonthFontSize: 16,
+    textMonthFontFamily: "HelveticaNeue",
+    textMonthFontWeight: "bold",
+    // day names
+    textSectionTitleColor: "black",
+    textDayHeaderFontSize: 12,
+    textDayHeaderFontFamily: "HelveticaNeue",
+    textDayHeaderFontWeight: "normal",
+    // dates
+    dayTextColor: "#00AAAF",
+    todayTextColor: "#af0078",
+    textDayFontSize: 18,
+    textDayFontFamily: "HelveticaNeue",
+    textDayFontWeight: "500",
+    textDayStyle: { marginTop: Platform.OS === "android" ? 2 : 4 },
+    // selected date
+    selectedDayBackgroundColor: "#00AAAF",
+    selectedDayTextColor: "white",
+    // disabled date
+    textDisabledColor: "#f2f7f7",
+    // dot (marked date)
+    dotColor: "#00AAAF",
+    selectedDotColor: "white",
+    disabledDotColor: "#f2f7f7",
+    dotStyle: { marginTop: -2 },
+  };
+  const today = new Date().toISOString().split("T")[0];
+  const [view, setView] = useState([]);
+  const [load, setLoad] = useState(false);
+  const [selectDate, setSelectDate] = useState();
+  const [mark, setMark] = useState({});
+  const [weekview, setweekview] = useState(false);
 
-const Calendar = ({navigation, route}) => {
-    const testIDs = {
-      menu: {
-        CONTAINER: 'menu',
-        CALENDARS: 'calendars_btn',
-        CALENDAR_LIST: 'calendar_list_btn',
-        HORIZONTAL_LIST: 'horizontal_list_btn',
-        AGENDA: 'agenda_btn',
-        EXPANDABLE_CALENDAR: 'expandable_calendar_btn',
-        WEEK_CALENDAR: 'week_calendar_btn',
-        TIMELINE_CALENDAR: 'timeline_calendar_btn',
-        PLAYGROUND: 'playground_btn'
-      },
-      calendars: {
-        CONTAINER: 'calendars',
-        FIRST: 'first_calendar',
-        LAST: 'last_calendar'
-      },
-      calendarList: {CONTAINER: 'calendarList'},
-      horizontalList: {CONTAINER: 'horizontalList'},
-      agenda: {
-        CONTAINER: 'agenda',
-        ITEM: 'item'
-      },
-      expandableCalendar: {CONTAINER: 'expandableCalendar'},
-      weekCalendar: {CONTAINER: 'weekCalendar'}
-    };
+  const info = (item) => {
+    navigation.navigate("DetailTask", {
+      task: item.data,
+    });
+  };
+  console.log("change");
+  useEffect(() => {
+    console.log("items");
+    async function x() {
+      let d = await AsyncStorage.getItem("selectdate");
+      let tasks = await AsyncStorage.getItem("tasks");
+      tasks = JSON.parse(tasks);
 
-    const theme = {
-      // arrows
-      arrowColor: 'black',
-      arrowStyle: {padding: 0},
-      // knob
-      expandableKnobColor: '#00AAAF',
-      // month
-      monthTextColor: 'black',
-      textMonthFontSize: 16,
-      textMonthFontFamily: 'HelveticaNeue',
-      textMonthFontWeight: 'bold',
-      // day names
-      textSectionTitleColor: 'black',
-      textDayHeaderFontSize: 12,
-      textDayHeaderFontFamily: 'HelveticaNeue',
-      textDayHeaderFontWeight: 'normal',
-      // dates
-      dayTextColor: '#00AAAF',
-      todayTextColor: '#af0078',
-      textDayFontSize: 18,
-      textDayFontFamily: 'HelveticaNeue',
-      textDayFontWeight: '500' ,
-      textDayStyle: {marginTop: Platform.OS === 'android' ? 2 : 4},
-      // selected date
-      selectedDayBackgroundColor: '#00AAAF',
-      selectedDayTextColor: 'white',
-      // disabled date
-      textDisabledColor: '#f2f7f7',
-      // dot (marked date)
-      dotColor: '#00AAAF',
-      selectedDotColor: 'white',
-      disabledDotColor: '#f2f7f7',
-      dotStyle: {marginTop: -2}
-    }
-    const today = new Date().toISOString().split('T')[0];
-    const [view, setView] = useState([]);
-    const[load, setLoad] = useState(false);
-    const [selectDate, setSelectDate] = useState();
-    const [mark,setMark] = useState({});
-    const [weekview,setweekview] = useState(false);
+      tasks = tasks.task;
+      // console.log(tasks);
+      let _mark = {};
+      let data = { repeat: [] };
+      let _view = [{ title: d, data: [] }];
+      for (const i of tasks) {
+        if (!i.create_at && !i.due && !i.repeat) continue;
+        let timestamp = "";
+        if (i.create_at) timestamp = i.create_at;
+        if (i.due && !i.repeat) timestamp = i.due;
 
+        if (i.reminder && !i.repeat) timestamp = i.reminder;
+        let _date = timestamp.split("T");
+        let temp = new Date(timestamp);
 
-    const info = (item) => {
+        let _hour = temp.toTimeString().split(":");
+        if (i.repeat) {
+          let t = new Date(i.repeat.hour);
+          let _timestamp = new Date(timestamp);
 
-      navigation.navigate('DetailTask' ,{
-        task:item.data
-      })
-    }
-    console.log("change");
-    useEffect(()=> {
-      console.log("items");
-      async function x() {
-        let d = await AsyncStorage.getItem('selectdate');
-        let tasks = await AsyncStorage.getItem('tasks');
-        tasks = JSON.parse(tasks);
-        
-        tasks= tasks.task;
-        // console.log(tasks);
-        let _mark = {};
-        let data = {repeat:[]};
-        let _view = [{title:d, data:[]}];
-        for (const i of tasks) {
-          if(!i.create_at && !i.due && !i.repeat) continue;
-          let timestamp = "";
-          if(i.create_at) timestamp = i.create_at
-          if (i.due && !i.repeat)timestamp =  i.due
-
-          if(i.reminder && !i.repeat) timestamp = i.reminder
-          let _date = timestamp.split('T');
-          let temp = new Date(timestamp);
-          
-          let _hour = temp.toTimeString().split(':');
-          if(i.repeat) {
-
-            let t = new Date(i.repeat.hour);
-            let _timestamp = new Date(timestamp);
-
-            _timestamp.setHours(t.getHours());
-            _timestamp.setMinutes(t.getMinutes());
-            timestamp = _timestamp.toISOString();
-            _hour = _timestamp.toTimeString().split(':');
-            console.log(_hour);
-            data["repeat"].push({hour: _hour[0] + ":" + _hour[1], title: i.repeat.type + "-" + i.title, data: i, timestamp:timestamp, repeat: i.repeat})
-            continue;
-          }
-          if (!data[_date[0]]) {
-
-            data[_date[0]] = [{hour: _hour[0] + ":" + _hour[1], title: i.title, data: i, timestamp:timestamp}];
-            
-          } else {
-            data[_date[0]].push({hour: _hour[0] + ":" + _hour[1], title: i.title,data: i, timestamp:timestamp});
-          }
+          _timestamp.setHours(t.getHours());
+          _timestamp.setMinutes(t.getMinutes());
+          timestamp = _timestamp.toISOString();
+          _hour = _timestamp.toTimeString().split(":");
+          console.log(_hour);
+          data["repeat"].push({
+            hour: _hour[0] + ":" + _hour[1],
+            title: i.repeat.type + "-" + i.title,
+            data: i,
+            timestamp: timestamp,
+            repeat: i.repeat,
+          });
+          continue;
         }
-        let _data = [];
-        function compareDate(a, b) {
-          let a_date = new Date(a.timestamp).getTime();
-          let b_date = new Date(b.timestamp).getTime();
-          return a_date - b_date;
+        if (!data[_date[0]]) {
+          data[_date[0]] = [
+            {
+              hour: _hour[0] + ":" + _hour[1],
+              title: i.title,
+              data: i,
+              timestamp: timestamp,
+            },
+          ];
+        } else {
+          data[_date[0]].push({
+            hour: _hour[0] + ":" + _hour[1],
+            title: i.title,
+            data: i,
+            timestamp: timestamp,
+          });
         }
-        _view[0].data = data[d] || [];
-        console.log(_view);
-        console.log(data[d]);
-        for (const k in data) {
-          if (Object.hasOwnProperty.call(data, k)) {
-            const element = data[k];
-            _mark[k] = {marked:true}
-          }
-        }
-        
-
-        for (const i of data["repeat"]) {
-          console.log("test");
-          if (i.repeat.type.includes("Daily")) {
-            
-            let t = new Date(i.repeat.hour);
-            let _timestamp = new Date(d);
-            _timestamp.setHours(t.getHours());
-            _timestamp.setMinutes(t.getMinutes());
-            let temp = i;
-            temp.timestamp = _timestamp.toISOString();
-            _view[0].data.push(temp);
-          }
-          if (i.repeat.type.includes("Weekly")) {
-            let t = new Date(i.repeat.hour);
-            let _timestamp = new Date(d);
-            _timestamp.setHours(t.getHours());
-            let temp = i;
-            temp.timestamp = _timestamp.toISOString();
-            if(_timestamp.getDay() ==1) _view[0].data.push(temp);
-            
-          }
-          if (i.repeat.type.includes("Monthly")) {
-            let t = new Date(i.repeat.hour);
-            let _timestamp = new Date(d);
-            _timestamp.setHours(t.getHours());
-            let temp = i;
-            temp.timestamp = _timestamp.toISOString();
-            if(_timestamp.getDate() ==15) _view[0].data.push(temp);
-          }
-        };
-        _view[0].data.sort(compareDate);
-        console.log(_view[0].data);
-        setLoad(true);
-        setMark(_mark);
-        setSelectDate(d);
-        setView(_view);
-        
       }
-      x();
-      if(load) return;
-      console.log("effect");
+      let _data = [];
+      function compareDate(a, b) {
+        let a_date = new Date(a.timestamp).getTime();
+        let b_date = new Date(b.timestamp).getTime();
+        return a_date - b_date;
+      }
+      _view[0].data = data[d] || [];
+      console.log(_view);
+      console.log(data[d]);
+      for (const k in data) {
+        if (Object.hasOwnProperty.call(data, k)) {
+          const element = data[k];
+          _mark[k] = { marked: true };
+        }
+      }
 
-      // getTask();
-    },[selectDate])
+      for (const i of data["repeat"]) {
+        console.log("test");
+        if (i.repeat.type.includes("Daily")) {
+          let t = new Date(i.repeat.hour);
+          let _timestamp = new Date(d);
+          _timestamp.setHours(t.getHours());
+          _timestamp.setMinutes(t.getMinutes());
+          let temp = i;
+          temp.timestamp = _timestamp.toISOString();
+          _view[0].data.push(temp);
+        }
+        if (i.repeat.type.includes("Weekly")) {
+          let t = new Date(i.repeat.hour);
+          let _timestamp = new Date(d);
+          _timestamp.setHours(t.getHours());
+          let temp = i;
+          temp.timestamp = _timestamp.toISOString();
+          if (_timestamp.getDay() == 1) _view[0].data.push(temp);
+        }
+        if (i.repeat.type.includes("Monthly")) {
+          let t = new Date(i.repeat.hour);
+          let _timestamp = new Date(d);
+          _timestamp.setHours(t.getHours());
+          let temp = i;
+          temp.timestamp = _timestamp.toISOString();
+          if (_timestamp.getDate() == 15) _view[0].data.push(temp);
+        }
+      }
+      _view[0].data.sort(compareDate);
+      console.log(_view[0].data);
+      setLoad(true);
+      setMark(_mark);
+      setSelectDate(d);
+      setView(_view);
+    }
+    x();
+    if (load) return;
+    console.log("effect");
 
+    // getTask();
+  }, [selectDate]);
 
-
-     const onDateChanged = useCallback( async (date, updateSource) => {
-    console.log('ExpandableCalendarScreen onDateChanged: ', date, updateSource);
-      setSelectDate(date);
-      // setView([]);
-      console.log(selectDate);
-      await AsyncStorage.setItem('selectdate',date);
+  const onDateChanged = useCallback(async (date, updateSource) => {
+    console.log("ExpandableCalendarScreen onDateChanged: ", date, updateSource);
+    setSelectDate(date);
+    // setView([]);
+    console.log(selectDate);
+    await AsyncStorage.setItem("selectdate", date);
   }, []);
-    function renderItem  ({item}) {
-      return <AgendaItem item={item} info={info}/>;
-    };
+  function renderItem({ item }) {
+    return <AgendaItem item={item} info={info} />;
+  }
 
-      return (
-        <CalendarProvider
-          style={innerHeight="90%"}
-          date={today}
-          // onDateChanged={(date)=> {
-          //   setSelectDate(date)
+  return (
+    <CalendarProvider
+      style={(innerHeight = "90%")}
+      date={today}
+      // onDateChanged={(date)=> {
+      //   setSelectDate(date)
 
+      // }}
+      onDateChanged={onDateChanged}
+      // onMonthChange={onMonthChange}
+      showTodayButton
+      // disabledOpacity={0.6}
+      // theme={todayBtnTheme.current}
+      // todayBottomMargin={16}
+    >
+      <View style={styles.header}>
+        <Text style={styles.titleScreen}>Calendar</Text>
+        <FontAwesome name="calendar" size={40} color="purple" />
+      </View>
 
-          // }}
-          onDateChanged={onDateChanged}
-          // onMonthChange={onMonthChange}
-          showTodayButton
-          // disabledOpacity={0.6}
-          // theme={todayBtnTheme.current}
-          // todayBottomMargin={16}
-        > 
-        {
-          weekview  ? (<View>
-            <Text style={{textAlign:'center', fontSize:'18px'}}>--</Text>
-            <Text style={{textAlign:'center', fontSize:'18px'}}>{selectDate ? new Date(selectDate).toString().split(' ')[1]  + " " + new Date(selectDate).toString().split(' ')[3]: "----"}</Text>
-          <WeekCalendar
-            testID={testIDs.expandableCalendar.CONTAINER}/>
+      {weekview ? (
+        <View style={styles.calendarWeek}>
+          <View style={{ alignItems: "center" }}>
+            <View style={styles.framecalendarWeek}>
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: "18px",
+                  color: "blue",
+                  marginVertical: 5,
+                }}
+              >
+                {selectDate
+                  ? new Date(selectDate).toString().split(" ")[1] +
+                    " " +
+                    new Date(selectDate).toString().split(" ")[3]
+                  : "----"}
+              </Text>
+            </View>
           </View>
-          ) : (
-            <View>
-              <Text style={{textAlign:'center', fontSize:'18px'}}>--</Text>
+
+          <WeekCalendar testID={testIDs.expandableCalendar.CONTAINER} />
+        </View>
+      ) : (
+        <View style={styles.calendarMonth}>
           <ExpandableCalendar
             testID={testIDs.expandableCalendar.CONTAINER}
-
             // horizontal={false}
             // hideArrows
             disablePan
@@ -249,38 +278,64 @@ const Calendar = ({navigation, route}) => {
             // animateScroll
             // closeOnDayPress={false}
           />
-          </View>
-          )
-        }
+        </View>
+      )}
 
-          <Button
-          onPress={()=>{setweekview(!weekview)}}
-          >Change to {weekview ? "Month View" : "Week View"}</Button>
-          <AgendaList
-            sections={view}  
-            renderItem={renderItem}
-            // scrollToNextEvent
-            sectionStyle={styles.section}
-            // dayFormat={'yyyy-MM-d'}
-          />
-        </CalendarProvider>
-      );
-    }
-
-
+      <Button
+        onPress={() => {
+          setweekview(!weekview);
+        }}
+      >
+        Change to {weekview ? "Month View" : "Week View"}
+      </Button>
+      <AgendaList
+        sections={view}
+        renderItem={renderItem}
+        // scrollToNextEvent
+        sectionStyle={styles.section}
+        // dayFormat={'yyyy-MM-d'}
+      />
+    </CalendarProvider>
+  );
+};
 
 const styles = StyleSheet.create({
-  calendar: {
-    paddingLeft: 20,
-    paddingRight: 20
+  calendarMonth: {
+    marginVertical: 5,
   },
+  calendarWeek: {},
   header: {
-    backgroundColor: 'lightgrey'
+    marginTop: 50,
+    alignContent: "center",
+    alignItems: "center",
+  },
+  titleScreen: {
+    fontWeight: "bold",
+    fontSize: 30,
   },
   section: {
-    backgroundColor: '#f2f7f7',
-    color: 'grey',
-    textTransform: 'capitalize'
-  }
+    backgroundColor: "#f2f7f7",
+    color: "grey",
+    textTransform: "capitalize",
+  },
+  framecalendarWeek: {
+    alignItems: "center",
+    marginVertical: 10,
+    width: "25%",
+    height: 40,
+    backgroundColor: "#efefef",
+    borderRadius: 5,
+    ...Platform.select({
+      ios: {
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.5,
+        shadowRadius: 3.84,
+      },
+      android: {
+        elevation: 25,
+      },
+    }),
+  },
 });
 export default Calendar;
